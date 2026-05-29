ab52185b7321a65b1798bf14bf2893d0b2186620
Thomas <alabythomas@gmail.com>
2026-05-28 21:26:08 -0400

    creation de l'inventaire au format json

--- fruit_manager.py
+++ fruit_manager.py
@@ -1,10 +1,13 @@
-inventaire = {
-    "banane":120,
-    "mangues":85,
-    "ananas":45,
-    "noix de coco":60,
-    "papayes":30
-}
+import json 
+
+def ouvrir_inventaire(path="inventaire.json"):
+    with open(path,'r',encoding='utf-8') as fichier:
+        inventaire = json.load(fichier)
+    return inventaire
+
+def ecrire_inventaire(inventaire,path="inventaire.json"):
+    with open(path,'w',encoding='utf-8') as fichier:
+        json.dump(inventaire,fichier,ensure_ascii= False,indent = 4)
 
 def afficher_inventaire(inventaire):
     print("Inventaire actuel de la plantation:")
