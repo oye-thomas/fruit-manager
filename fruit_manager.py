d4c0bf18ba3625db84735c5046aa9136f6779bc1
Thomas <alabythomas@gmail.com>
2026-06-01 18:39:45 -0400

    ajout de la fonctionalite de tresorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -5,9 +5,25 @@ def ouvrir_inventaire(path="inventaire.json"):
         inventaire = json.load(fichier)
     return inventaire
 
-def ecrire_inventaire(inventaire,path="inventaire.json"):
+def ecrire_inventaire(inventaire,path="data/inventaire.json"):
     with open(path,'w',encoding='utf-8') as fichier:
         json.dump(inventaire,fichier,ensure_ascii= False,indent = 4)
+
+def ouvrir_tresorerie(path="data/tresorerie.txt"):
+    with open(path,'r',encoding='utf-8') as fichier:
+        tresorerie = json.load(fichier)
+        return tresorerie
+
+def ecrire_tresorerie(tresorerie ,path="data/tresorerie.txt"):
+    with open(path,'w',encoding='utf-8') as fichier:
+        json.dump(tresorerie,fichier,ensure_ascii=False,indent=4)
+        
+    
+def afficher_tresorerie(tresorerie):
+    print(f"/n💰 Trésorerie actuelle :{tresorerie:2f}$")
+    
+    
+    
 
 def afficher_inventaire(inventaire):
     print("Inventaire actuel de la plantation:")
@@ -18,16 +34,22 @@ def recolter(inventaire,fruit,quantite):
     inventaire[fruit] = inventaire.get(fruit,0) + quantite 
     print(f"f\n✅ Recolté {quantite} {fruit} supplementaire !")
     
-def vendre(inventaire,fruit,quantite):
+def vendre(inventaire,fruit,quantite, tresorerie):
     if inventaire.get(fruit,0) >= quantite:
         inventaire[fruit] -= quantite
+        tresorerie += 1*quantite
         print(f"\n💰 Vendu {quantite} {fruit} !")
+        return (inventaire,tresorerie)
     else:
         print(f"\n❌ Pas assez de {fruit} pour vendre {quantite} unités")
 
 if __name__ == "__main__":
     inventaire = ouvrir_inventaire()
+    tresorerie = ouvrir_tresorerie()
+    afficher_inventaire(tresorerie)
     afficher_inventaire(inventaire)
+    
     recolter(inventaire,"bananes",10)
-    vendre(inventaire,"bananes",5)
-    afficher_inventaire(inventaire)
+    inventaire, tresorerie = vendre(inventaire,'bananes',5)
+    ecrire_inventaire(inventaire)
+    ecrire_tresorerie(tresorerie)
